5be656e3d5af1f60efd3ceb3a25ae2d1595dd123
Bernard Teye <datssosh@gmail.com>
2024-10-04 21:14:51 +0000

    add OTP component

---
+++ src/components/molecules/OTP.tsx
@@ -0,0 +1,12 @@
+export default function OTP() {
+  return (
+    <div className="flex gap-2 w-[inherit] overflow-hidden">
+      <input type="text" className="p-2 border border-[#d0d0d0] rounded-lg w-16 text-center text-4xl" />
+      <input type="text" className="p-2 border border-[#d0d0d0] rounded-lg w-16 text-center text-4xl" />
+      <input type="text" className="p-2 border border-[#d0d0d0] rounded-lg w-16 text-center text-4xl" />
+      <input type="text" className="p-2 border border-[#d0d0d0] rounded-lg w-16 text-center text-4xl" />
+      <input type="text" className="p-2 border border-[#d0d0d0] rounded-lg w-16 text-center text-4xl" />
+      <input type="text" className="p-2 border border-[#d0d0d0] rounded-lg w-16 text-center text-4xl" />
+    </div>
+  );
+}
